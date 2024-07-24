22c255cedf4e19d1cc83f76f1b35e13ccbf89555
hajarhjr <hamimhajar85@gmail.com>
2024-07-24 10:45:16 +0100

    add user and event entity

---
+++ hello-events/src/main/java/com/Entity/User.java
@@ -0,0 +1,29 @@
+package com.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+    private String email;
+    private String role;
+
+}
